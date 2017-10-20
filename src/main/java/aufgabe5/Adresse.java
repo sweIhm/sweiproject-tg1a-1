cd90cf4bc5e57f3580bb58e2d12cf136a788e7c1
xXxH4n5Wur5t360NoPullsxXx <32670263+xXxH4n5Wur5t360NoPullsxXx@users.noreply.github.com>
2017-10-20 12:36:19 +0200

    Update Adresse.java

--- src/main/java/aufgabe5/Adresse.java
+++ src/main/java/aufgabe5/Adresse.java
@@ -1,11 +1,8 @@
 package aufgabe5;
 
-import java.util.List;
-
 public class Adresse {
     private String strasse;
     private int hausnummer;
     private String plz;
     private String ort;
-    List<Kunden> kunden;
 }
