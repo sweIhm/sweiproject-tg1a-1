1a4201fd0b3774d9008c56f7da2bad1eb2ae1fc7
Dimitri Krivoj <dimikriv1@live.com>
2017-10-20 12:42:40 +0200

    aufgabe 5

--- src/main/java/aufgabe5/Konto.java
+++ src/main/java/aufgabe5/Konto.java
@@ -1,11 +1,21 @@
 package aufgabe5;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Konto {
 
     private String bezeichnung;
-    private GeldBetrag geldBetrag;
+    private GeldBetrag geldBetrag = new GeldBetrag();
+    private List<Kunde> zeichnungsberechtigte = new ArrayList<Kunde>();
 
-
+    public Konto(List<Kunde> zeichnungsberechtigte, String bezeichnung) {
+        if (zeichnungsberechtigte.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        this.zeichnungsberechtigte.addAll(zeichnungsberechtigte);
+        this.bezeichnung = bezeichnung;
+    }
 
     public GeldBetrag saldo() {
         return geldBetrag;
