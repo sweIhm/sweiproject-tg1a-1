a143fd660e9b894a4c7664a569802f7c9a54b8c2
xXxH4n5Wur5t360NoPullsxXx <32670263+xXxH4n5Wur5t360NoPullsxXx@users.noreply.github.com>
2017-11-24 15:24:31 +0100

    Fixed application not starting properly

--- backend/src/main/java/edu/hm/cs/se/activitymeter/ActivityMeter.java
+++ backend/src/main/java/edu/hm/cs/se/activitymeter/ActivityMeter.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ActivityMeter {
     public static void main(String[] args) {
-        SpringApplication.run(ActivityController.class, args);
+        SpringApplication.run(ActivityMeter.class, args);
     }
 }
