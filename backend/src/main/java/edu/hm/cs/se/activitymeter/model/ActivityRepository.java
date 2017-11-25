340c3f2d12a1daea7284d1f912043fb5061bb41b
xXxH4n5Wur5t360NoPulls <gneumann@hm.edu>
2017-11-25 19:01:50 +0100

    Rearranged classes Made beautiful jpa classes Made fancy email-send-code Fixer upper of anything

--- backend/src/main/java/edu/hm/cs/se/activitymeter/model/ActivityRepository.java
+++ backend/src/main/java/edu/hm/cs/se/activitymeter/model/ActivityRepository.java
@@ -1,6 +1,7 @@
-package edu.hm.cs.se.activitymeter;
+package edu.hm.cs.se.activitymeter.model;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface ActivityRepository extends CrudRepository<Activity, Long> {
+	Iterable<Activity> findAllByPublished(boolean published);
 }
