ddfa94f013c04002de544cf524cd4a8dbe01b736
xXxH4n5Wur5t360NoPulls <gneumann@hm.edu>
2017-11-28 22:40:55 +0100

    Revert "Added Data Transfer Object for activity to hide personal data from unauthorized access"
    This reverts commit f0932af6c1d4241af646b6e69368278b7f4129da.

--- backend/src/main/java/edu/hm/cs/se/activitymeter/controller/ActivityController.java
+++ backend/src/main/java/edu/hm/cs/se/activitymeter/controller/ActivityController.java
@@ -1,6 +1,9 @@
 package edu.hm.cs.se.activitymeter.controller;
 import edu.hm.cs.se.activitymeter.controller.email.EmailController;
-import edu.hm.cs.se.activitymeter.model.*;
+import edu.hm.cs.se.activitymeter.model.ActivationKey;
+import edu.hm.cs.se.activitymeter.model.ActivationKeyRepository;
+import edu.hm.cs.se.activitymeter.model.ActivityRepository;
+import edu.hm.cs.se.activitymeter.model.Activity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
@@ -30,16 +33,16 @@ public class ActivityController {
     }
 
     @GetMapping("{id}")
-    public ActivityDTO find(@PathVariable Long id) {
-        return new ActivityDTO(activityRepository.findOne(id));
+    public Activity find(@PathVariable Long id) {
+        return activityRepository.findOne(id);
     }
 
     @PostMapping
-    public ActivityDTO create(@RequestBody Activity input) {
+    public Activity create(@RequestBody Activity input) {
         Activity newActivity = activityRepository.save(new Activity(input.getText(), input.getTitle(), input.getAuthor(), input.getEmail(), false));
         ActivationKey activationKey = activationKeyRepository.save(new ActivationKey(newActivity.getId(), emailController.generateKey()));
         emailController.sendEmail(newActivity, activationKey.getKey());
-        return new ActivityDTO(newActivity);
+        return newActivity;
     }
 
     @DeleteMapping("{id}")
@@ -48,7 +51,7 @@ public class ActivityController {
     }
 
     @PutMapping("{id}")
-    public ActivityDTO update(@PathVariable Long id, @RequestBody ActivityDTO input) {
+    public Activity update(@PathVariable Long id, @RequestBody Activity input) {
         Activity activity = activityRepository.findOne(id);
         if (activity == null) {
             return null;
@@ -56,7 +59,9 @@ public class ActivityController {
             activity.setText(input.getText());
             activity.setTitle(input.getTitle());
             activity.setAuthor(input.getAuthor());
-            return new ActivityDTO(activityRepository.save(activity));
+            activity.setEmail(input.getEmail());
+            activity.setPublished(input.isPublished());
+            return activityRepository.save(activity);
         }
     }
 
