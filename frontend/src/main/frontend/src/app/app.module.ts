f0932af6c1d4241af646b6e69368278b7f4129da
xXxH4n5Wur5t360NoPulls <gneumann@hm.edu>
2017-11-28 22:37:22 +0100

    Added Data Transfer Object for activity to hide personal data from unauthorized access

--- frontend/src/main/frontend/src/app/app.module.ts
+++ frontend/src/main/frontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ActivitiesComponent } from './activities/activities.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivitiesComponent
   ],
   imports: [
     BrowserModule,
