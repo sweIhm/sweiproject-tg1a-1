23fa67af53cd14569cdee1809fe4d17145fbbae9
xXxH4n5Wur5t360NoPulls <gneumann@hm.edu>
2017-11-28 22:47:24 +0100

    Added Data Transfer Object for activity to hide personal data from unauthorized access

---
+++ backend/src/main/java/edu/hm/cs/se/activitymeter/model/ActivityDTO.java
@@ -0,0 +1,68 @@
+package edu.hm.cs.se.activitymeter.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Activity")
+public class ActivityDTO {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="activity_id_seq")
+	@SequenceGenerator(name="activity_id_seq", sequenceName="activity_id_seq", allocationSize=1)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "text", nullable = false)
+	private String text;
+
+	@Column(name = "title", nullable = false)
+	private String title;
+
+	@Column(name = "author", nullable = false)
+	private String author;
+
+	public ActivityDTO (){}
+
+	public ActivityDTO(String text, String title, String author) {
+		this.text = text;
+		this.title = title;
+		this.author = author;
+	}
+
+	public ActvityDTO(Activity a) {
+		this(a.getText(), a.getTitle(), a.getAuthor());
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+}
