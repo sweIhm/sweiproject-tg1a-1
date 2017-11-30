7ae3c0798a635be062c6517e670dcbe69cb891b1
hm-Llukas <leon-lukas11@web.de>
2017-11-30 14:21:21 +0100

    email

--- backend/src/main/java/edu/hm/cs/se/activitymeter/controller/ActivationController.java
+++ backend/src/main/java/edu/hm/cs/se/activitymeter/controller/ActivationController.java
@@ -1,9 +1,14 @@
 package edu.hm.cs.se.activitymeter.controller;
 
 import edu.hm.cs.se.activitymeter.model.ActivationKey;
+<<<<<<< HEAD
 import edu.hm.cs.se.activitymeter.model.ActivationKeyRepository;
 import edu.hm.cs.se.activitymeter.model.Activity;
 import edu.hm.cs.se.activitymeter.model.ActivityRepository;
+=======
+import edu.hm.cs.se.activitymeter.model.KeyRepo;
+import edu.hm.cs.se.activitymeter.model.PostRepository;
+>>>>>>> f883b29... email
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,14 +17,22 @@ import org.springframework.web.bind.annotation.*;
 public class ActivationController {
 
     @Autowired
+<<<<<<< HEAD
     private ActivationKeyRepository keyrepo;
 
     @Autowired
     private ActivityRepository actirepo;
+=======
+    private KeyRepo keyrepo;
+
+    @Autowired
+    private PostRepository postrepo;
+>>>>>>> f883b29... email
 
     @GetMapping("{id}")
     public boolean activate (@PathVariable Long id, @RequestParam(name = "key", defaultValue = "") String key) {
         ActivationKey activationKey = keyrepo.findOne(id);
+<<<<<<< HEAD
             if (activationKey != null && key.equals(activationKey.getKey())) {
                 activationKey.getActivity().setPublished(true);
                 actirepo.save(activationKey.getActivity());
@@ -29,3 +42,14 @@ public class ActivationController {
         return false;
     }
 }
+=======
+        if (activationKey != null && key.equals(activationKey.getKey())) {
+            activationKey.getPost().setPublished(true);
+            postrepo.save(activationKey.getPost());
+            keyrepo.delete(id);
+            return true;
+        }
+        return false;
+    }
+}
+>>>>>>> f883b29... email
--- backend/src/main/java/edu/hm/cs/se/activitymeter/model/ActivationKey.java
+++ backend/src/main/java/edu/hm/cs/se/activitymeter/model/ActivationKey.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 @Table(name = "Keys")
 public class ActivationKey {
 
+<<<<<<< HEAD
 	@Column(name = "key", unique = true, nullable = false)
 	private String key;
 
@@ -44,3 +45,43 @@ public class ActivationKey {
 		return activity;
 	}
 }
+=======
+    @Column(name = "key", unique = true, nullable = false)
+    private String key;
+
+    @Id
+    @Column(name = "id")
+    private long post_id;
+
+    @OneToOne
+    @JoinColumn(name = "id")
+    private Post post;
+
+    public ActivationKey() {}
+
+    public ActivationKey(long post_id, String key) {
+        this.key = key;
+        this.post_id = post_id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public long getActivity_id() {
+        return post_id;
+    }
+
+    public void setActivity_id(long activity_id) {
+        this.post_id = activity_id;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+}
+>>>>>>> f883b29... email
