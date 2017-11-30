9d6f52c9da8a4d28fa6655c96b35983358a54b63
Dimitri Krivoj <dimikriv1@live.com>
2017-11-30 17:32:03 +0100

    Big fat merge

--- backend/src/main/java/edu/hm/cs/se/activitymeter/model/ActivityRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.hm.cs.se.activitymeter.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ActivityRepository extends CrudRepository<Activity, Long> {
-	Iterable<Activity> findAllByPublished(boolean published);
-}
