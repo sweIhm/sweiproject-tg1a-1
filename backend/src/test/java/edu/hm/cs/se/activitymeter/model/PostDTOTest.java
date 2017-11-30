43b8f27771d5fbc7528af0fb53a128e158edb3b6
xXxH4n5Wur5t360NoPulls <gneumann@hm.edu>
2017-11-30 21:30:30 +0100

    Added tests for constructors and getter and setters

--- backend/src/test/java/edu/hm/cs/se/activitymeter/model/PostDTOTest.java
+++ backend/src/test/java/edu/hm/cs/se/activitymeter/model/PostDTOTest.java
@@ -15,4 +15,26 @@ public class PostDTOTest {
         assertEquals(dto.getId(), p.getId());
     }
 
+    @Test
+    public void createDTOPost() throws Exception {
+        PostDTO p = new PostDTO("text", "title", "author");
+        assertEquals("text", p.getText());
+        assertEquals("title", p.getTitle());
+        assertEquals("author", p.getAuthor());
+    }
+
+    @Test
+    public void getterAndSetter() throws Exception {
+        PostDTO p = new PostDTO("text", "title", "author");
+        p.setAuthor("herbert");
+        p.setText("textus");
+        p.setTitle("titlus");
+        p.setId(1000L);
+
+        assertEquals("textus", p.getText());
+        assertEquals("titlus", p.getTitle());
+        assertEquals("herbert", p.getAuthor());
+        assertEquals(1000L, (long)p.getId());
+    }
+
 }
