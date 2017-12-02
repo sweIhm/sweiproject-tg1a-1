ae5e492633fe15de33e6d2fd672192d85b62c021
xXxH4n5Wur5t360NoPulls <gneumann@hm.edu>
2017-12-02 21:19:40 +0100

    Basic construct for new testing method

--- backend/src/test/java/edu/hm/cs/se/activitymeter/controller/ActivityControllerTest.java
+++ backend/src/test/java/edu/hm/cs/se/activitymeter/controller/ActivityControllerTest.java
@@ -1,11 +1,18 @@
 package edu.hm.cs.se.activitymeter.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import edu.hm.cs.se.activitymeter.ActivityMeter;
+import edu.hm.cs.se.activitymeter.model.Post;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -29,24 +36,49 @@ public class ActivityControllerTest {
     MockMvc mvc;
 
     @Autowired
-    PostRepository p;
+    JdbcTemplate db;
+
+    @Before
+    public void setUp() throws Exception {
+        db.execute("DROP TABLE Activity;");
+        db.execute("CREATE TABLE Activity(" +
+                "id INTEGER PRIMARY KEY," +
+                "title VARCHAR(255) NOT NULL," +
+                "text VARCHAR(255) NOT NULL," +
+                "author VARCHAR(255) NOT NULL," +
+                "email VARCHAR(1000) NOT NULL," +
+                "published BOOLEAN NOT NULL);");
+        Post p = new Post("tet", "3id.b", "imdronei", "ioeir", true);
+        String j = postToJson(p);
+        System.out.println(j);
+        System.out.println(jsonToPost(j));
+    }
 
     @Test
     public void listAll() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/activity")).andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().json("[{\"id\":1,\"text\":\"TestText\",\"title\":\"TestTitel\",\"author\":\"TestAutor\"}]")).andReturn();
     }
 
     @Test
     public void find() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/activity/1")).andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().json("{\"id\":1,\"text\":\"TestText\",\"title\":\"TestTitel\",\"author\":\"TestAutor\"}")).andReturn();
     }
 
     @Test
     public void update() throws Exception {
-        String original = "{\"id\":1,\"text\":\"TestText\",\"title\":\"TestTitel\",\"author\":\"TestAutor\"}";
-        String update = "{\"id\":1,\"text\":\"TestText2\",\"title\":\"TestTite\",\"author\":\"TestMutter\"}";
-        mvc.perform(MockMvcRequestBuilders.put("/activity/1").contentType("application/json").content(update)).andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().json(update)).andReturn();
-        mvc.perform(MockMvcRequestBuilders.get("/activity/1")).andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().json(update)).andReturn();
-        mvc.perform(MockMvcRequestBuilders.put("/activity/1").contentType("application/json").content(original)).andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().json(original)).andReturn();
     }
+
+    private void addPostToDB(Post p) {
+        db.execute(String.format("INSERT INTO VALUES(%d,%s,%s,%s,%s%s);",
+                p.getId(), p.getTitle(), p.getText(), p.getAuthor(), p.getEmail(), p.isPublished()));
+    }
+
+    private String postToJson(Post p) throws Exception {
+        ObjectWriter w = new ObjectMapper().writer();
+        return w.writeValueAsString(p);
+    }
+
+    private Post jsonToPost(String p) throws Exception {
+        return new ObjectMapper().reader(Post.class).readValue(p);
+    }
+
+
 }
