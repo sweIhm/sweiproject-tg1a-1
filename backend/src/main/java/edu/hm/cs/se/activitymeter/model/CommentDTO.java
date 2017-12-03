ed67cc8aeb57a7591a27964af326d871f9170629
xXxH4n5Wur5t360NoPulls <gneumann@hm.edu>
2017-12-03 22:11:06 +0100

    Fixed CommentDTO indentation

--- backend/src/main/java/edu/hm/cs/se/activitymeter/model/CommentDTO.java
+++ backend/src/main/java/edu/hm/cs/se/activitymeter/model/CommentDTO.java
@@ -2,47 +2,47 @@ package edu.hm.cs.se.activitymeter.model;
 
 public class CommentDTO {
 
-    private Long id;
+  private Long id;
 
-    private String text;
+  private String text;
 
-    private String author;
+  private String author;
 
-    public CommentDTO() {
-        // Leerer Konstruktor für JPA
-    }
+  public CommentDTO() {
+    // Leerer Konstruktor für JPA
+  }
 
-    public CommentDTO(String text, String title, String author, String email, boolean published) {
-        this.text = text;
-        this.author = author;
-    }
+  public CommentDTO(String text, String title, String author, String email, boolean published) {
+    this.text = text;
+    this.author = author;
+  }
 
-    public CommentDTO(Comment k) {
-        this.text = k.getText();
-        this.author = k.getAuthor();
-    }
+  public CommentDTO(Comment k) {
+    this.text = k.getText();
+    this.author = k.getAuthor();
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getAuthor() {
-        return author;
-    }
+  public String getAuthor() {
+    return author;
+  }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+  public void setAuthor(String author) {
+    this.author = author;
+  }
 
-    public String getText() {
-        return text;
-    }
+  public String getText() {
+    return text;
+  }
 
-    public void setText(String text) {
-        this.text = text;
-    }
+  public void setText(String text) {
+    this.text = text;
+  }
 }
