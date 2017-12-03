ca9aa85ebc9777516891187c8ed2ad42e8043929
xXxH4n5Wur5t360NoPulls <gneumann@hm.edu>
2017-12-03 21:27:07 +0100

    Renamed Configuration to fit convention

--- backend/src/main/java/edu/hm/cs/se/activitymeter/configuration/ProductionConfiguration.java
+++ backend/src/main/java/edu/hm/cs/se/activitymeter/configuration/ProductionConfiguration.java
@@ -1,16 +1,15 @@
 package edu.hm.cs.se.activitymeter.configuration;
 
 import edu.hm.cs.se.activitymeter.controller.email.EmailController;
-import edu.hm.cs.se.activitymeter.controller.email.FakeEmailController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("default")
-public class testConfiguration {
+@Profile({"prod", "test"})
+public class ProductionConfiguration {
 	@Bean
-	public static EmailController newFakeEmailController() {
-		return new FakeEmailController();
+	public static EmailController newEmailController() {
+		return new EmailController();
 	}
 }
--- backend/src/main/java/edu/hm/cs/se/activitymeter/configuration/TestConfiguration.java
+++ backend/src/main/java/edu/hm/cs/se/activitymeter/configuration/TestConfiguration.java
@@ -1,15 +1,16 @@
 package edu.hm.cs.se.activitymeter.configuration;
 
 import edu.hm.cs.se.activitymeter.controller.email.EmailController;
+import edu.hm.cs.se.activitymeter.controller.email.FakeEmailController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile({"prod", "test"})
-public class productionConfiguration {
+@Profile("default")
+public class TestConfiguration {
 	@Bean
-	public static EmailController newEmailController() {
-		return new EmailController();
+	public static EmailController newFakeEmailController() {
+		return new FakeEmailController();
 	}
 }
