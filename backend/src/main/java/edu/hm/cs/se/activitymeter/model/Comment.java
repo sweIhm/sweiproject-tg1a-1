dc94e389664214cb22faba994061d478e0d4e1aa
Leon <leon-lukas11@web.de>
2017-12-07 19:29:32 +0100

    checkstyle

--- backend/src/main/java/edu/hm/cs/se/activitymeter/model/Comment.java
+++ backend/src/main/java/edu/hm/cs/se/activitymeter/model/Comment.java
@@ -1,6 +1,16 @@
 package edu.hm.cs.se.activitymeter.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
 
 @Entity
 @Table(name = "Comment")
--- backend/src/main/java/edu/hm/cs/se/activitymeter/model/Post.java
+++ backend/src/main/java/edu/hm/cs/se/activitymeter/model/Post.java
@@ -1,6 +1,15 @@
 package edu.hm.cs.se.activitymeter.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
